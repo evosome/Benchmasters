4b845d1a40132a63484c7eb5484542165ad6f36d
evosome <alex.evosome@gmail.com>
2024-07-29 15:32:35 +0400

    Add item heap

---
+++ Benchmasters/Items/ItemHeap.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Benchmasters.Items
+{
+    /// <summary>
+    /// Represents a bunch of items with the same type and quantity value.
+    /// </summary>
+    public class ItemHeap
+    {
+        public ItemType Type { get; }
+        public int Quantity { get; }
+
+        public bool IsEmpty => Quantity == 0;
+
+        public ItemHeap(ItemType type) : this(type, 0) { }
+
+        public ItemHeap(ItemType type, int quantity)
+        {
+            Type = type;
+            Quantity = quantity;
+        }
+    }
+}
