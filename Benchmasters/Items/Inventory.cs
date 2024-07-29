565b789d85cff9da659e23c9c3c88caecb0fb041
evosome <alex.evosome@gmail.com>
2024-07-29 16:22:24 +0400

    Add inventory to game

---
+++ Benchmasters/Items/Inventory.cs
@@ -0,0 +1,92 @@
+﻿
+using System;
+using System.Linq;
+
+namespace Benchmasters.Items
+{
+    public class Inventory
+    {
+        private readonly ItemHeap[] _itemHeaps;
+
+        public Inventory(int capacity)
+        {
+            _itemHeaps = new ItemHeap[capacity];
+            Array.Fill(_itemHeaps, default);
+        }
+
+        private ItemHeap FindFirstEmpty()
+        {
+            return _itemHeaps.First((heap) => heap.IsEmpty);
+        }
+
+        private ItemHeap FindWithSameType(ItemType type)
+        {
+            return _itemHeaps.First((heap) => heap.Type == type);
+        }
+
+        /// <summary>
+        /// Push item heap to inventory. 
+        /// </summary>
+        /// <param name="heap">Item heap to push</param>
+        /// <returns>true, if pushed, otherwise false</returns>
+        public bool Push(ItemHeap heap)
+        {
+            if (heap.IsEmpty)
+            {
+                return false;
+            }
+
+            // try to merge with the heap of same type
+            var sameHeap = FindWithSameType(heap.Type);
+            if (sameHeap != null)
+            {
+                sameHeap.Merge(heap);
+                return true;
+            }
+
+            // or try replace with empty heap
+            var emptyHeap = FindFirstEmpty();
+            if (emptyHeap != null)
+            {
+                emptyHeap.ReplaceWith(heap);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Add item to inventory at the certain index. Empty heaps not allowed to add.
+        /// </summary>
+        /// <param name="heap">Item heap to add</param>
+        /// <param name="index">Index to add to</param>
+        /// <returns>true, if added, otherwise false</returns>
+        public bool Add(ItemHeap heap, int index)
+        {
+            if (heap.IsEmpty)
+            {
+                return false;
+            }
+
+            var certainHeap = Get(index);
+            if (certainHeap != null && certainHeap.Type == heap.Type)
+            {
+                certainHeap.Merge(heap);
+                return true;
+            }
+
+            return false;
+        }
+
+        public ItemHeap Get(int index)
+        {
+            return _itemHeaps[index];
+        }
+
+        public ItemHeap Pop(int index)
+        {
+            var certainHeap = Get(index);
+            return certainHeap.WithdrawAll();
+        }
+    }
+}
--- Benchmasters/Items/ItemHeap.cs
+++ Benchmasters/Items/ItemHeap.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace Benchmasters.Items
 {
     /// <summary>
@@ -6,8 +8,8 @@ namespace Benchmasters.Items
     /// </summary>
     public class ItemHeap
     {
-        public ItemType Type { get; }
-        public int Quantity { get; }
+        public ItemType Type { get; private set; }
+        public int Quantity { get; private set; }
 
         public bool IsEmpty => Quantity == 0;
 
@@ -17,6 +19,59 @@ namespace Benchmasters.Items
         {
             Type = type;
             Quantity = quantity;
+        }
+
+        /// <summary>
+        /// Merge other item heap into this one. Heaps must be
+        /// with the same item type.
+        /// </summary>
+        /// <param name="heap">Other heap.</param>
+        public void Merge(ItemHeap heap)
+        {
+            if (heap.Type != Type)
+            {
+                return;
+            }
+            Quantity += heap.Quantity;
+            heap.WithdrawAll();
+        }
+
+        /// <summary>
+        /// Change item type and item quantity to values of other heap. Other
+        /// item heap will be empty. This item heap can be replaced with other, when it's empty.
+        /// </summary>
+        /// <param name="heap">Heap to replace with</param>
+        public void ReplaceWith(ItemHeap heap)
+        {
+            if (Quantity != 0)
+            {
+                return;
+            }
+
+            Type = heap.Type;
+            Quantity = heap.Quantity;
+            heap.WithdrawAll();
+        }
+
+        /// <summary>
+        /// Withdraw all.
+        /// </summary>
+        /// <returns>Splitted off item heap</returns>
+        public ItemHeap WithdrawAll()
+        {
+            return Withdraw(Quantity);
+        }
+
+        /// <summary>
+        /// Withdraw the certain amount.
+        /// </summary>
+        /// <param name="amount">Amount to withdraw</param>
+        /// <returns>Splitted off item heap</returns>
+        public ItemHeap Withdraw(int amount)
+        {
+            amount = Math.Min(amount, Quantity);
+            Quantity -= amount;
+            return new ItemHeap(Type, amount);
         }
     }
 }
